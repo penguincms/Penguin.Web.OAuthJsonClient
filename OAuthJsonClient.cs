4ff56f7167dadcea755ed92d3134b91f2453bc87
anloffredo <anloffredo@massageenvy.com>
2021-02-03 13:24:55 -0700

    Auto Commit

--- OAuthJsonClient.cs
+++ OAuthJsonClient.cs
@@ -1,6 +1,7 @@
 ﻿using Penguin.Web;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net;
 using System.Text;
 
@@ -12,31 +13,49 @@ namespace Penguin.Web
     {
         private OAuthToken Token { get; set; }
 
+        protected override void PreRequest(Uri url)
+        {
+            if (!this.Headers.AllKeys.Contains("Authorization"))
+            {
+                ApplyAuthHeader(this, this.Token);
+            }
+        }
+
+        public static void ApplyAuthHeader(WebClient client, OAuthToken token)
+        {
+            if (client is null)
+            {
+                throw new ArgumentNullException(nameof(client));
+            }
+
+            if (token is null)
+            {
+                throw new ArgumentNullException(nameof(token));
+            }
+
+            client.Headers.Add("Authorization", $"{token.TokenType} {token.AccessToken}");
+        }
+
         public OAuthJsonClient(string Key, string Secret, string AccessUrl, string Username = null, string Password = null)
         {
             string encoded = System.Convert.ToBase64String(System.Text.Encoding.GetEncoding("ISO-8859-1").GetBytes(Key + ":" + Secret));
 
-            WebClient client = new WebClient();
-
-            client.Headers.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-            client.Headers.Add("Authorization", "Basic " + encoded);
-
-            string data;
-
-            if(!string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password))
+            using (WebClient client = new WebClient())
             {
-                data = $"grant_type=password&username={Username}&password={Password}";
+
+                client.Headers.Add("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+                client.Headers.Add("Authorization", "Basic " + encoded);
+
+                string data = !string.IsNullOrWhiteSpace(Username) && !string.IsNullOrWhiteSpace(Password)
+                    ? $"grant_type=password&username={Username}&password={Password}"
+                    : $"grant_type=client_credentials";
+
+                string response = client.UploadString(AccessUrl, data);
+
+                this.Token = Newtonsoft.Json.JsonConvert.DeserializeObject<OAuthToken>(response);
+
+                ApplyAuthHeader(this, this.Token);
             }
-            else
-            {
-                data = $"grant_type=client_credentials";
-            }
-
-            string response = client.UploadString(AccessUrl, data);
-
-            Token = Newtonsoft.Json.JsonConvert.DeserializeObject<OAuthToken>(response);
-
-            this.Headers.Add("Authorization", $"{Token.token_type} {Token.access_token}");
         }
 
 
--- OAuthToken.cs
+++ OAuthToken.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,9 +7,46 @@ namespace Penguin.Web
 {
     public class OAuthToken
     {
-        public string access_token { get; set; }
-        public int expires_in { get; set; }
-        public string scope { get; set; }
-        public string token_type { get; set; }
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+
+        [JsonProperty("expires_in")]
+        public int ExpiresIn { get; set; }
+
+        [JsonProperty("scope")]
+        public string Scope { get; set; }
+
+        [JsonProperty("token_type")]
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Globalization", "CA1304:Specify CultureInfo", Justification = "<Pending>")]
+        public string TokenType
+        {
+            get
+            {
+                if (this.tokenType is null)
+                {
+                    return null;
+                }
+
+                string toReturn = string.Empty;
+
+
+                for (int i = 0; i < this.tokenType.Length; i++)
+                {
+                    if (i == 0)
+                    {
+                        toReturn += char.ToUpper(this.tokenType[i]);
+                    }
+                    else
+                    {
+                        toReturn += this.tokenType[i];
+                    }
+                }
+
+                return toReturn;
+            }
+            set => this.tokenType = value;
+        }
+
+        private string tokenType;
     }
 }
